c6489862cc018be5dea05605fcba49326d5f307f
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-09-27 21:45:20 +0500

    Integrate Axios and showed the data on UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])  
+  const API_URL = 'http://localhost:3000/api/users'
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      console.log(response);
+      setUsers(response.data.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container mx-auto p-4 ">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center bg-blue-50 p-4 rounded-md">CRUD Using React and Express</h1>
+      <button 
+        onClick={fetchUsers} 
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+      >
+        Fetch Users
+      </button>
+      <ul className="mt-4">
+        <li className="grid grid-cols-4 gap-4 font-bold">
+          <span>ID</span>
+          <span>Name</span>
+          <span>Email</span>
+          <span>Age</span>
+        </li>
+        {users.map((user) => (
+          <li key={user.id} className="grid grid-cols-4 gap-4 border-b py-2">
+            <span>{user.id}</span>
+            <span>{user.name}</span>
+            <span>{user.email}</span>
+            <span>{user.age}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
