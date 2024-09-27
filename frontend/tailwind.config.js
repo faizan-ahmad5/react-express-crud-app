4933a6fafdb0babf024fc1fa0af95bbbe01ffa7b
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-09-27 22:30:58 +0500

    completed the frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add this line to include all JS, JSX, TS, and TSX files in the src directory
+    // ... other paths if necessary
+  ],
   theme: {
     extend: {},
   },
